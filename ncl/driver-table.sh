10d0919fc4628cbd832ff8a0ed796dede8ebbd27
zarzycki <czarzycki@psu.edu>
2020-06-15 15:49:06 -0400

    more refactoring of table code to split units rows into two subtables

--- ncl/driver-table.sh
+++ ncl/driver-table.sh
@@ -21,9 +21,9 @@ else
   BASINTXT=${BASIN}
 fi
 
-ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=False calc_deltas=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_spatial_corr.csv"' 'plot_title="'${DESCSTR}' spatial correlation ('${BASINTXT}')"' 
-ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=True calc_deltas=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_spatial_rmse.csv"' 'plot_title="'${DESCSTR}' spatial RMSE ('${BASINTXT}')"' 
-ncl plot-table.ncl plot_bias=True relative_performance=False invert_stoplight=False calc_deltas=True 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_climo_mean.csv"' 'plot_title="'${DESCSTR}' climatological bias ('${BASINTXT}')"'
-ncl plot-table.ncl plot_bias=True relative_performance=False invert_stoplight=False calc_deltas=True 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_storm_mean.csv"' 'plot_title="'${DESCSTR}' storm mean bias ('${BASINTXT}')"'
-ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=False calc_deltas=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_temporal_scorr.csv"' 'plot_title="'${DESCSTR}' seasonal correlation ('${BASINTXT}')"'
+ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=False calc_deltas=False write_units=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_spatial_corr.csv"' 'plot_title="'${DESCSTR}' spatial correlation ('${BASINTXT}')"' 
+ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=True calc_deltas=False write_units=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_spatial_rmse.csv"' 'plot_title="'${DESCSTR}' spatial RMSE ('${BASINTXT}')"' 
+ncl plot-table.ncl plot_bias=True relative_performance=False invert_stoplight=False calc_deltas=True write_units=True 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_climo_mean.csv"' 'plot_title="'${DESCSTR}' climatological bias ('${BASINTXT}')"'
+ncl plot-table.ncl plot_bias=True relative_performance=False invert_stoplight=False calc_deltas=True write_units=True 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_storm_mean.csv"' 'plot_title="'${DESCSTR}' storm mean bias ('${BASINTXT}')"'
+ncl plot-table.ncl plot_bias=False relative_performance=True invert_stoplight=False calc_deltas=False write_units=False 'csvfilename="metrics_'${THECSVFILE}'_'${BASIN}'_temporal_scorr.csv"' 'plot_title="'${DESCSTR}' seasonal correlation ('${BASINTXT}')"'
 
