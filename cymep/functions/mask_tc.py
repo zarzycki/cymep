e684e23f0b214a3b929da3f447acf2e36ee12559
zarzycki <czarzycki@psu.edu>
2021-03-26 11:22:38 -0400

    add hemi option in mask_tc

--- cymep/functions/mask_tc.py
+++ cymep/functions/mask_tc.py
@@ -1,34 +1,43 @@
-def maskTC(lat,lon):
+def maskTC(lat,lon,dohemi=False):
 
+  # dohemi defaults to False, so basin-by-basin
+  # however, if overrode with dohemi = True, will stratify by NHEMI and SHEMI
+  
   # if lon is negative, switch to 0->360 convention
   if lon < 0.0:
     lon = lon + 360.
 
-  # Coefficients for calculating ATL/EPAC sloped line
-  m = -0.58
-  b = 0. -m*295.
-  maxlat = 50.0
-
-  if lat >= 0. and lat <= maxlat and lon > 257. and lon <= 359.:
-    funcval = m*lon + b
-    if lat > funcval:
-      basin = 1
+  if dohemi == True:
+    if lat >= 0.:
+      basin = 20
     else:
-      basin = 2
-  elif lat >= 0. and lat <= maxlat  and lon > 220. and lon <= 257.:
-    basin = 2
-  elif lat >= 0. and lat <= maxlat  and lon > 180. and lon <= 220.:
-    basin = 3
-  elif lat >= 0. and lat <= maxlat  and lon > 100. and lon <= 180.:
-    basin = 4
-  elif lat >= 0. and lat <= maxlat  and lon > 30.  and lon <= 100.:
-    basin = 5
-  elif lat  < 0. and lat >= -maxlat and lon > 30.  and lon <= 135.:
-    basin = 6
-  elif lat  < 0. and lat >= -maxlat and lon > 135. and lon <= 290.:
-    basin = 7
+      basin = 21
   else:
-    basin = 0
+    # Coefficients for calculating ATL/EPAC sloped line
+    m = -0.58
+    b = 0. -m*295.
+    maxlat = 50.0
+
+    if lat >= 0. and lat <= maxlat and lon > 257. and lon <= 359.:
+      funcval = m*lon + b
+      if lat > funcval:
+        basin = 1
+      else:
+        basin = 2
+    elif lat >= 0. and lat <= maxlat  and lon > 220. and lon <= 257.:
+      basin = 2
+    elif lat >= 0. and lat <= maxlat  and lon > 180. and lon <= 220.:
+      basin = 3
+    elif lat >= 0. and lat <= maxlat  and lon > 100. and lon <= 180.:
+      basin = 4
+    elif lat >= 0. and lat <= maxlat  and lon > 30.  and lon <= 100.:
+      basin = 5
+    elif lat  < 0. and lat >= -maxlat and lon > 30.  and lon <= 135.:
+      basin = 6
+    elif lat  < 0. and lat >= -maxlat and lon > 135. and lon <= 290.:
+      basin = 7
+    else:
+      basin = 0
 
   return basin
   
@@ -63,6 +72,10 @@ def getbasinmaskstr(gridchoice):
         strbasin="SATL"
       elif gridchoice == 9:
         strbasin="FLA"
+      elif gridchoice == 20:
+        strbasin="NHEMI"
+      elif gridchoice == 21:
+        strbasin="SHEMI"
       else:
         strbasin="NONE"
 
