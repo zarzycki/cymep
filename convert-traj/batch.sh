4cf3cbc9e48719f481af338323666b1018ca8996
Colin Zarzycki <zarzycki@ucar.edu>
2023-08-04 10:09:58 -0400

    parallelize HRMIP conversion code

--- convert-traj/batch.sh
+++ convert-traj/batch.sh
@@ -1,12 +1,32 @@
-#!/bin/bash
+#!/bin/bash -l
 
-source activate ncl_stable
+##### ICDS ROAR
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=12:00:00
+#PBS -A open
+#PBS -j oe
+#PBS -N hrmip-process
 
-#BASEDIR=/Users/cmz5202/Downloads/highresmip/
-#BASEDIR=~/PSUGDrive/HighResMIP/
-BASEDIR=~/Downloads/HRMIP/
+#module load ncl
+module load parallel
+NUMCORES=10
+
+SOFTPATH=/storage/home/cmz5202/sw/cymep/convert-traj
+
+TIMESTAMP=`date +%s%N`
+COMMANDFILE=commands.${TIMESTAMP}.txt
+
+cd ${SOFTPATH}
+
+BASEDIR=/storage/home/cmz5202/group/highresmip
 ALLFILES=`find ${BASEDIR} -name "*-?H_*nc"`
 echo $ALLFILES
 for f in $ALLFILES; do
-  echo ncl highresmip-to-tempest.ncl 'fname="'$f'"'
+  NCLCOMMAND="ncl highresmip-to-tempest.ncl
+      'fname=\"'$f'\"' "
+  echo ${NCLCOMMAND} >> ${COMMANDFILE}
 done
+
+parallel --jobs ${NUMCORES} --workdir $PWD < ${COMMANDFILE}
+
+rm -v ${COMMANDFILE}
